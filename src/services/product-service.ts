921109faedb8ba65cd3d78ac9873d0d23f4a894a
thiago rabaioli <tfrabaioli@outlook.com>
2025-08-05 10:19:25 +0100

    first commit

---
+++ src/services/product-service.ts
@@ -0,0 +1,55 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { ProductDTO } from "../models/product";
+import { requestBackend } from "../utils/requests";
+import { BASE_URL } from "../utils/system";
+
+export function findPageRequest(page: number, name: string, size = 12, sort = "description") {
+    const config : AxiosRequestConfig = {
+        method: "GET",
+        url: "/itemlosts",
+        params: {
+            page,
+            name,
+            size,
+            sort 
+        }
+    }
+
+    return requestBackend(config);
+}
+
+export function findById(id: number) {
+    return requestBackend({ url: `/itemlosts/${id}` });
+}
+
+export function deleteById(id: number) {
+    const config : AxiosRequestConfig = {
+        method: "DELETE",
+        url: `/itemlosts/${id}`,
+        withCredentials: true
+    }
+
+    return requestBackend(config);
+}
+
+export function updateRequest(obj: ProductDTO) {
+    const config: AxiosRequestConfig = {
+        method: "PUT",
+        url: `/itemlosts/${obj.id}`,
+        withCredentials: true,
+        data: obj
+    }
+
+    return requestBackend(config);
+}
+
+export function insertRequest(obj: ProductDTO) {
+    const config: AxiosRequestConfig = {
+        method: "POST",
+        url: "/itemlosts",
+        withCredentials: true,
+        data: obj
+    }
+
+    return requestBackend(config);
+}
