1ac36112631b09b1caabd6dd49993b70253ef2c4
thiago rabaioli <tfrabaioli@outlook.com>
2025-08-16 18:37:57 +0100

    batch lote delivery  not work

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,14 +1,17 @@
+import { UserDTO } from "./user";
+
+// A classe OrderDTO pode continuar a existir se for usada noutra parte do sistema.
 export class OrderDTO {
   id?: number;
   items: OrderItemDTO[] = [];
-
 }
 
-export class OrderItemDTO {
-  constructor(
-    public itemlostId: number,
-    public description: string,
-    public imgUrl: string
-  ) {}
-  
+// ESTA É A CORREÇÃO PRINCIPAL
+// Transformámos a classe antiga num 'type' que corresponde ao DTO do backend.
+export type OrderItemDTO = {
+    id: number;
+    type: number;
+    notes: string;
+    interactionDate: string; // O 'Instant' do Java é serializado como string
+    user: UserDTO; // O objeto do utilizador que realizou a ação
 }
