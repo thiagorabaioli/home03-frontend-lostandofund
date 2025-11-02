8ba68f51bd2ab6e205a7222de8012b18b96cf81d
thiagorabaioli <tfrabaioli@outlook.com>
2025-11-02 22:09:58 +0000

    request change /api

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,17 +1,16 @@
 import { UserDTO } from "./user";
 
-// A classe OrderDTO pode continuar a existir se for usada noutra parte do sistema.
+
 export class OrderDTO {
   id?: number;
   items: OrderItemDTO[] = [];
 }
 
-// ESTA É A CORREÇÃO PRINCIPAL
-// Transformámos a classe antiga num 'type' que corresponde ao DTO do backend.
+
 export type OrderItemDTO = {
     id: number;
     type: number;
     notes: string;
-    interactionDate: string; // O 'Instant' do Java é serializado como string
-    user: UserDTO; // O objeto do utilizador que realizou a ação
+    interactionDate: string; 
+    user: UserDTO; 
 }
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -14,22 +14,22 @@ export function requestBackend(config: AxiosRequestConfig) {
   return axios({ ...config, baseURL: BASE_URL, headers });
 }
 
-// REQUEST INTERCEPTOR
+
 axios.interceptors.request.use(
   function (config) {
-    // DO SOMETHING BEFORE REQUEST IS SENT
+  
     return config;
   },
   function (error) {
-    // DO SOMETHING WITH REQUEST ERROR
+  
     return Promise.reject(error);
   }
 );
 
-// RESPONSE INTERCEPTOR
+
 axios.interceptors.response.use(
   function (response) {
-    // DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+   
     return response;
   },
   function (error) {
